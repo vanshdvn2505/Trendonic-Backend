9c31b993a57842c007c1d16870bc42df2a5f0caa
Vansh Dhawan <152591281+vanshdvn2505@users.noreply.github.com>
2024-07-17 20:09:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ connectToDatabase();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-    origin: 'https://trendonic.netlify.app/',
+    origin: 'https://main--trendonic.netlify.app',
     methods: ['GET', 'POST', 'OPTIONS', 'PUT'],
     allowedHeaders: ['Content-Type'],
     credentials: true
